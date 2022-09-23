c8992302e239b76945aad71ce37561383c52a9c3
Thomas Vermaut <40355758+tvermaut@users.noreply.github.com>
2022-09-23 17:04:31 +0200

    init

---
+++ script.js
@@ -0,0 +1,14 @@
+const q = window.location.search;
+if(q.length>1){
+    fetch('https://oat.hisgis.nl/oat-ws/rest/scans/' + q.substring(1))
+        .then((response) => response.json())
+        .then((data) => verwerkScans(data));
+}
+
+
+function verwerkScans(scans){
+secties = [];
+for(let si in scans){
+    console.log(si.code);
+}
+}
