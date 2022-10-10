756a72e61d75a15fdea4895f9ee72af3cea6e378
Thomas Vermaut <40355758+tvermaut@users.noreply.github.com>
2022-10-10 11:39:32 +0200

    text-right

--- script.js
+++ script.js
@@ -67,13 +67,13 @@ function getOppervlakHTML(opp){
     let r = ((opp-e) % 10000)/100;
     let b = (opp-e-(100*r)) /10000;
     let e_h = document.createElement("td");
-    e_h.setAttribute("class","pl-0");
+    e_h.setAttribute("class","pl-0 text-right");
     e_h.innerHTML = (e == 0 && r == 0 && b == 0 ? "" : String(e).padStart(2,'0'));
     let r_h = document.createElement("td");
-    r_h.setAttribute("class","px-0");
+    r_h.setAttribute("class","px-0 text-right");
     r_h.innerHTML = (r == 0 && b == 0 ? "" : String(r).padStart(2,'0'));
     let b_h = document.createElement("td");
-    b_h.setAttribute("class","pr-0");
+    b_h.setAttribute("class","pr-0 text-right");
     b_h.innerHTML = (b == 0 ? "" : b);
     h.push(b_h);
     h.push(r_h);
