cd288b11041f4d7f769756c7fd739416f5a9ce1d
Thomas Vermaut <40355758+tvermaut@users.noreply.github.com>
2022-10-10 10:59:37 +0200

    test

--- script.js
+++ script.js
@@ -26,8 +26,8 @@ function verwerkScan(s){
         pi.appendChild(pnr);
 
         var aantal_a = 1;
-        let huidig = parseInt(px);
-        var i = 1;
+        //let huidig = parseInt(px);
+        //var i = 1;
         //while(p_sort.length >= (huidig+i) && (p_sort[huidig]).artikelLink.artikelnr == p_sort[huidig+i].artikelLink.artikelnr){aantal_a++; i++}
         let aid = p.artikelLink.artikelnr;
         if(p.artikelLink.artikelnrtvg){aid += p.artikelLink.artikelnrtvg;}
