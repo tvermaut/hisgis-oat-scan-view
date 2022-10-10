c67db94c9bc5742737ee5915e859f54c71f95c46
Thomas Vermaut <40355758+tvermaut@users.noreply.github.com>
2022-10-10 09:53:36 +0200

    rowspan

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ function verwerkScan(s){
         if(a.artikelnrtvg){artikelid += a.artikelnrtvg;}
         artikelen[artikelid] = a;
         }
-    let p_sort = s.results.sort((p1, p2) => (p1.perceelnr < p2.perceelnr) ? -1 : (p1.perceelnr > p2.perceelnr) ? 1 : 0);        
+    var p_sort = s.results.sort((p1, p2) => (p1.perceelnr < p2.perceelnr) ? -1 : (p1.perceelnr > p2.perceelnr) ? 1 : 0);        
     for(let px in p_sort){
         let p = p_sort[px];
         let pi = document.createElement("tr");
@@ -29,6 +29,7 @@ function verwerkScan(s){
         console.log("test: ");
         console.log(px);
         console.log((p_sort[px]).artikelLink);
+        console.log((p_sort[px++]).artikelLink);
         while(p_sort.length >= px++ && (p_sort[px]).artikelLink.artikelnr == p_sort[px++].artikelLink.artikelnr){aantal_a++;}
         let aid = p.artikelLink.artikelnr;
         if(p.artikelLink.artikelnrtvg){aid += p.artikelLink.artikelnrtvg;}
