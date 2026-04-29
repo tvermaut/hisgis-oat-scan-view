957edeaa37fcb1e36202158124ce4e5cc969b04d
Thomas Vermaut <40355758+tvermaut@users.noreply.github.com>
2026-04-29 19:27:24 +0200

    consorten voluit

--- script.js
+++ script.js
@@ -173,7 +173,7 @@ function getArtikelHTML(a, aantal){
     anr = '<span class="badge artikelnr py-1 px-2 me-1">' + anr + '</span>';
 
     var cs = '';
-    if(a.cs === 'true'){
+    if(a.consorten === 'true'){
         cs = ' <span class="badge consorten py-1 px-2 me-1">c.s.</span>';
     }
 
